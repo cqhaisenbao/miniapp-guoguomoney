1783518bbf08dc755f8f633a44ee41257e2edad4
cqhaisenbao <fanyu9353@gmail.com>
2020-12-25 12:27:20 +0800

    封装了微信登录函数

--- pages/tabbar/my/my.vue
+++ pages/tabbar/my/my.vue
@@ -1,14 +1,52 @@
 <template>
 	<view>
-		
+		<button open-type="getUserInfo">获取用户信息</button>
+		<view class="userinfo">
+			<view class="userinfo-avatar">
+				<open-data type="userAvatarUrl"></open-data>
+			</view>
+			<open-data type="userNickName"></open-data>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default{
+	export default {
+		data() {
+			return {}
+		},
+		onLoad() {
+			const db = uniCloud.database();
+			let res=db.collection('recordList').where('uid==$env.uid').get().then(res=>console.log(res))
+		},
+		methods: {
+
+		},
+
 	}
 </script>
 
 <style lang="scss" scoped>
+	.userinfo {
+		position: relative;
+		width: 750rpx;
+		height: 320rpx;
+		color: black;
+		font-weight: 600;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 
+	.userinfo-avatar {
+		overflow: hidden;
+		display: block;
+		width: 120rpx;
+		height: 120rpx;
+		margin: 20rpx;
+		margin-top: 50rpx;
+		border-radius: 50%;
+		border: 2px solid #fff;
+		box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+	}
 </style>
--- pages/tabbar/static/static.vue
+++ pages/tabbar/static/static.vue
@@ -5,45 +5,50 @@
 </template>
 
 <script>
+	import wxLogin from '@/lib/weixinlogin'
 	let weixinAuthService
 	export default {
-		methods: {
-			getWeixinCode() {
-				return new Promise((resolve, reject) => {
-					uni.login({
-						provider: 'weixin',
-						success(res) {
-							resolve(res.code)
-						},
-						fail(err) {
-							reject(new Error('微信登录失败'))
-						}
-					})
-				})
-			},
-			loginByWeixin() {
-				this.getWeixinCode().then((code) => {
-					console.log(code)
-					return uniCloud.callFunction({
-						name: 'login-by-weixin',
-						data: {code}
-					})
-				}).then((res) => {
-					uni.showModal({
-						showCancel: false,
-						content: JSON.stringify(res.result)
-					})
-					if (res.result.code === 0) {
-						uni.setStorageSync('uni_id_token', res.result.token)
-					}
-				}).catch(() => {
-					uni.showModal({
-						showCancel: false,
-						content: '微信登录失败，请稍后再试'
-					})
-				})
-			}
-		}
+		onLoad() {
+			wxLogin()
+		},
+		// methods: {
+		// 	getWeixinCode() {
+		// 		return new Promise((resolve, reject) => {
+		// 			uni.login({
+		// 				provider: 'weixin',
+		// 				success(res) {
+		// 					resolve(res.code)
+		// 				},
+		// 				fail(err) {
+		// 					reject(new Error('微信登录失败'))
+		// 				}
+		// 			})
+		// 		})
+		// 	},
+		// 	loginByWeixin() {
+		// 		this.getWeixinCode().then((code) => {
+		// 			console.log(code)
+		// 			return uniCloud.callFunction({
+		// 				name: 'login-by-weixin',
+		// 				data: {code}
+		// 			})
+		// 		}).then((res) => {
+		// 			console.log(res)
+		// 			uni.showModal({
+		// 				showCancel: false,
+		// 				content: JSON.stringify(res.result.message)
+		// 			})
+		// 			if (res.result.code === 0) {
+		// 				uni.setStorageSync('uni_id_token', res.result.token)
+		// 			}
+		// 		}).catch(() => {
+		// 			uni.showModal({
+		// 				showCancel: false,
+		// 				content: '微信登录失败，请稍后再试'
+		// 			})
+		// 		})
+		// 	}
+		// }
 	}
 </script>
 
