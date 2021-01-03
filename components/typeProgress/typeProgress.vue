1a1840316e32c831f90854524d5c895f93fe6e0e
cqhaisenbao <fanyu9353@gmail.com>
2021-01-03 16:31:15 +0800

    完成支出收入分类展示

--- components/typeProgress/typeProgress.vue
+++ components/typeProgress/typeProgress.vue
@@ -1,19 +1,74 @@
 <template>
 	<view>
-		<u-line-progress active-color="#3EB575" :percent="70"></u-line-progress>
+		<view class="content" v-for="(item,_id) in currentlist" :key="_id">
+			<view class="content_left">
+				<view :class='item.tagName' class="content_icon"></view>
+				<view class="content_text">{{item.tag}}</view>
+			</view>
+			<u-line-progress :show-percent="false" :height=20 class="line_progress" active-color="#3EB575" :percent="countPercent(item)"></u-line-progress>
+			<view>￥{{item.amount.toFixed(2)}}</view>
+		</view>
 	</view>
 </template>
 
 <script>
 	export default {
+		props: {
+			currentlist: {
+				type: Array
+			},
+			currentAmount: {
+				default: 0
+			},
+			amount_income: {
+				default: 0
+			}
+		},
 		data() {
 			return {
-				
 			};
+		},
+		methods: {
+			countPercent(item) {
+				return parseInt(item.amount / this.currentAmount * 100)
+			},
 		}
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.content {
+		height: 30px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 20px 12px;
 
+		.content_left {
+			display: flex;
+			justify-content: flex-start;
+			align-items: center;
+			margin-right: 10px;
+			width: 30vw;
+
+			.content_icon {
+				color: $main-color;
+				font-size: 24px;
+				margin-right: 12px;
+			}
+
+			.content_text {
+				color: black;
+				font-size: 14px;
+				line-height: 30px;
+			}
+		}
+
+		.line_progress {
+			@extend %df;
+			flex-shrink: 0;
+			flex-grow: 1;
+			margin-right: 10px;
+		}
+	}
 </style>
--- pages/tabbar/my/my.vue
+++ pages/tabbar/my/my.vue
@@ -2,7 +2,7 @@
 	<view>
 		<monthStatistics v-if="recordList" :nowmonth.sync="nowmonth" :amount_pay="amount_pay" :amount_income="amount_income"></monthStatistics>
 		<typeSelect :selectedType.sync='selectedType'></typeSelect>
-		<typeProgress></typeProgress>
+		<typeProgress :currentlist="selectedList_norepeat" :currentAmount="currentAmount" :amount_income="amount_income"></typeProgress>
 		<!-- <canvasBar :nowmonth='nowmonth' ></canvasBar> -->
 	</view>
 </template>
@@ -11,6 +11,7 @@
 	import dayjs from 'dayjs';
 	import { mapState, mapMutations, mapActions } from 'vuex';
 	import selectedListAmount from '@/lib/selectedListAmount.js'
+	import selectedMonthAmount from '@/lib/selectedMonthAmount.js'
 	export default {
 		data() {
 			return {
@@ -18,8 +19,9 @@
 				selectedType: '-',
 				amount_pay: 0,
 				amount_income: 0,
-				currentlist_pay: [],
-				currentlist_income: []
+				currentAmount:0,
+				selectedList: [],
+				selectedList_norepeat: []
 			}
 		},
 		computed: {
@@ -27,16 +29,55 @@
 		},
 		watch: {
 			recordList() {
-				selectedListAmount.call(this, this.nowmonth, '-')
-				console.log('recordList变了',this.currentlist_income)
+				selectedListAmount.call(this, this.nowmonth, this.selectedType)
+				selectedMonthAmount.call(this, this.nowmonth, '-')
+				this.handleCurrentList()
 			},
 			nowmonth() {
-				selectedListAmount.call(this, this.nowmonth, '-')
+				selectedListAmount.call(this, this.nowmonth, this.selectedType)
+				selectedMonthAmount.call(this, this.nowmonth, '-')
+				this.handleCurrentList()
+			},
+			selectedType(){
+				selectedListAmount.call(this, this.nowmonth, this.selectedType)
+				this.handleCurrentList()
 			}
 		},
 		created() {
-			selectedListAmount.call(this, this.nowmonth, '-')
+			selectedListAmount.call(this, this.nowmonth, this.selectedType)
+			selectedMonthAmount.call(this, this.nowmonth, '-')
+			this.handleCurrentList()
 		},
+		methods: {
+			handleCurrentList() {
+				this.selectedList = []
+				const list = this.recordList
+				const currenttime = this.nowmonth
+				for (let i = 0; i < list.length; i++) {
+					let time = dayjs(list[i].time).format('YYYY年MM月')
+					if (list[i].type === this.selectedType && time === this.nowmonth) {
+						// this.selectedList.push(list[i])
+						this.selectedList.push({
+							tag: list[i].tag,
+							amount: list[i].amount,
+							tagName:list[i].tagName
+						})
+					}
+				}
+				const newRecordList = []
+				this.selectedList.forEach(el => {
+					const result = newRecordList.findIndex(ol => { return el.tag === ol.tag })
+					if (result !== -1) {
+						newRecordList[result].amount = newRecordList[result].amount + el.amount
+					} else {
+						newRecordList.push(el)
+					}
+				})
+
+				this.selectedList_norepeat = newRecordList
+				console.log('处理去重后的表', newRecordList)
+			}
+		}
 	};
 </script>
 
